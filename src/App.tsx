5d03458b6e6e94fa197d2480a3781a2caec65f6b
BatoolNeyaz <batool.khan@shell.com>
2022-03-12 09:22:10 +0000

    created Interface for the Tutorial +AxiosTuServices with findAll,findById,findByTitle,create,update,deleteById,deleteAll

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,57 @@
-
+import React from "react";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Tutorial from "./interfaces/Tutorial";
+import AxiosTutServices from "./services/TutorialService";
+import { useQuery, useMutation } from "react-query";
+import { useState, useEffect } from "react";
+
 const App: React.FC = () => {
-  return (
-    <h1></h1>
+  //Setting initial states of GetId,GetTitle and GetResult using UseState react hook
+  const [getId, setGetId] = useState("");
+  const [getTitle, setGetTitle] = useState("");
+  const [getResult, setGetResult] = useState<string | null>(null);
+
+  //After receiveing response it should be converted into string.
+  const formatResponse = (res: any) => {
+    return JSON.stringify(res, null, 2);
+  };
+
+  const { isLoading: isLoadingTutorials, refetch: getAllTutorials } = useQuery<
+    Tutorial[],
+    Error
+  >(
+    "query-tutorials",
+    async () => {
+      return await AxiosTutServices.findAll();
+    },
+    {
+      enabled: false,
+      onSuccess: (res) => {
+        setGetResult(formatResponse(res));
+      },
+      onError:(err:any)=>{
+        setGetResult(formatResponse(err.response?.data|| err));
+      },
+    }
   );
-}
+
+  
+};
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+
+//           Learn React
+
+//       </header>
+//     </div>
+//   );
+// }
+
 export default App;
